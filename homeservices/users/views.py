e34c0399a03565e2940eebf0f3ba4d6aacce5d8e
Research Devkota <researchofficial55@gmail.com>
2025-02-16 23:16:52 +0545

    google signup method added.

--- homeservices/users/views.py
+++ homeservices/users/views.py
@@ -1,9 +1,44 @@
-# users/views.py
 from django.shortcuts import render, redirect
-from .models import User
-import random
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
-
+def index(request):
+    return render(request, 'index.html')
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'users/home.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Error in signup process')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+def feature(request):
+    return render(request, 'feature.html')
+
+def contact(request):
+    return render(request, 'contact.html')
