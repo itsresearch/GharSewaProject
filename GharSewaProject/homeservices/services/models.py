5900e953949a7d1d3aa2f63f489c32f077bd2f47
Research Devkota <researchofficial55@gmail.com>
2025-03-05 17:07:51 +0545

    error solved.

--- GharSewaProject/homeservices/services/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Service(models.Model):
-    name = models.CharField(max_length=100)  # e.g., Plumbing, Painting
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
